2d0f84c140ead6a2c20ccad3d8e755cc75355fed
jeanspaixao <jeanspaixao@gmail.com>
2022-12-22 13:45:17 -0300

    finishing challenge

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.time.LocalDate;
 
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -22,12 +20,31 @@ public class Main {
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
 
 
-    
 
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila: "+devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteúdos Concluídos Camila: "+devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João: "+devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos Concluídos João: "+devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
     }
 }
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -4,19 +4,18 @@ public abstract class Conteudo {
 
     protected static final double XP_PADRAO = 10d;
 
-    private String tituto;
+    private String titulo;
     private String descricao;
 
     public abstract double calcularXp();
 
-    
 
     public String getTituto() {
-        return tituto;
+        return titulo;
     }
 
-    public void setTituto(String tituto) {
-        this.tituto = tituto;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public String getDescricao() {
